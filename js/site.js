c8677995cac8399d8879629269922f178c269cf1
Andy Footner <andyfootner@netscape.net>
2019-06-03 17:34:19 +0200

    [yTZmniny] redo header/footer and remove jquery and bootstrap

---
+++ js/site.js
@@ -0,0 +1,100 @@
+/**
+ * Add visibility togglers for the some elements of the header and footer.
+ */
+(function () {
+  'use strict';
+
+  function toggle(button, collapse) {
+    var target = document.getElementById(button.getAttribute('aria-controls'));
+    var expanded = collapse || button.getAttribute('aria-expanded') === 'true';
+
+    // Switch the expanded/collapsed states.
+    button.setAttribute('aria-expanded', !expanded);
+    target.setAttribute('data-hidden', expanded);
+  }
+
+  function collapseAll(exception) {
+    var elements = document.querySelectorAll('[aria-expanded="true"]');
+    for (var i = 0, l = elements.length; i < l; i++) {
+      var element = elements[i];
+      if (element !== exception) {
+        toggle(elements[i], true);
+      }
+    }
+  }
+
+  function handleToggle(event) {
+    var target = event.target;
+    if (target) {
+      if (target.nodeName === 'SPAN') {
+        target = target.parentNode;
+      }
+      collapseAll(target);
+      toggle(target);
+    }
+  }
+
+  function handleOutsideClick(event) {
+    var target = event.target;
+    if (target) {
+      if (target.nodeName === 'A') {
+        collapseAll();
+      }
+      else if (target.hasAttribute) {
+        var body = document.body;
+        while (target && target.hasAttribute && !target.hasAttribute('aria-expanded') && !target.hasAttribute('data-hidden') && target !== body) {
+          target = target.parentNode;
+        }
+        if (target && target.hasAttribute && !target.hasAttribute('aria-expanded') && !target.hasAttribute('data-hidden')) {
+          collapseAll();
+        }
+      }
+    }
+  }
+
+  function setToggler(id, textAccessibility, textDisplay) {
+    var element = document.getElementById(id);
+
+console.log('element', element);
+    // Skip if the element was not found.
+    if (!element) {
+      return;
+    }
+
+    // Add the aria attribute to control visibility to the form.
+    element.setAttribute('data-hidden', true);
+
+    // Button label.
+    var label = document.createElement('span');
+    label.appendChild(document.createTextNode(textDisplay));
+
+    // Extended label for accessibility.
+    var accessibleLabel = document.createElement('span');
+    accessibleLabel.className = 'accessibility';
+    accessibleLabel.appendChild(document.createTextNode(textAccessibility));
+
+    // Create the button.
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('id', id + '-toggler');
+    button.setAttribute('aria-expanded', false);
+    button.setAttribute('aria-controls', id);
+    button.setAttribute('value', textDisplay);
+    button.appendChild(accessibleLabel);
+    button.appendChild(label);
+
+    // Add toggling function.
+    button.addEventListener('click', handleToggle);
+
+    // Add the button before the toggable element.
+    element.parentNode.insertBefore(button, element);
+  }
+console.log('classname', document.documentElement.className);
+  if (document.documentElement.className === 'js') {
+    // Collapse popups when clicking outside of the toggable target.
+    document.addEventListener('click', handleOutsideClick);
+
+    // Add the togglers.
+    setToggler('ocha-services', 'Toggle ', 'OCHA Services');
+  }
+})();
